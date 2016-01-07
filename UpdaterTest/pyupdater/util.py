44c00382f78297cabba63207a37ea59e722507d9
Ryan Pessa <ryan@essential-elements.net>
2016-01-07 12:55:41 -0600

    use org.kivy.android instead of org.renpy.android when appropriate

--- UpdaterTest/pyupdater/util.py
+++ UpdaterTest/pyupdater/util.py
@@ -1,14 +1,20 @@
 import os
-from jnius import autoclass, cast
+from jnius import autoclass, cast, JavaException
 import subprocess
 
-PythonActivity = autoclass('org.renpy.android.PythonActivity')
-PythonService = autoclass('org.renpy.android.PythonService')
+try:
+	PythonActivity = autoclass('org.kivy.android.PythonActivity')
+	PythonService = autoclass('org.kivy.android.PythonService')
+except JavaException:
+	PythonActivity = autoclass('org.renpy.android.PythonActivity')
+	PythonService = autoclass('org.renpy.android.PythonService')
+
 
 def mContext():
 	if PythonActivity.mActivity:
 		return cast('android.content.Context', PythonActivity.mActivity.getApplication())
 	return cast('android.content.Context', PythonService.mService.getApplication())
+
 
 def get_current_version():
 	print 'get_current_version called'
@@ -17,6 +23,7 @@ def get_current_version():
 	print 'package name', package_name
 	version_code = context.getPackageManager().getPackageInfo(package_name, 0).versionCode
 	return version_code
+
 
 def check_for_root():
 	print 'checking for root access'
